0ef721fc97dbb3287e83b27ae4a567af29a16a9d
Atsighi Bright <brytebee@gmail.com>
2021-07-09 14:13:49 +0100

    Add devise and users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts do
     resources :comments
   end
-
+  resources :devise
   root 'posts#index'
 
   get '/about', to: 'pages#about'
